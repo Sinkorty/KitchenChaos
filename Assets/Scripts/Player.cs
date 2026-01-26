94a5489efae3c6b4657677c14439de233a35a54c
Sinkorty <1103829278@qq.com>
2026-01-26 15:54:28 +0800

    添加了Player动画, 配置了Virtual Camerea

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,25 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     // Update is called once per frame
     void Update()
     {
         Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
         transform.position += moveDir * Time.deltaTime * moveSpeed;
 
-        if (moveDir != Vector3.zero)
-        {
-            transform.forward = moveDir;
-        }
+        //if (moveDir != Vector3.zero)
+        //{
+        //    transform.forward = moveDir;
+        //}
+        isWalking = moveDir != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Player player;
     private Animator animator;
 
     private void Awake()
@@ -12,6 +15,6 @@ public class PlayerAnimator : MonoBehaviour
     }
     private void Update()
     {
-        
+        animator.SetBool(IS_WALKING, player.IsWalking());
     }
 }
