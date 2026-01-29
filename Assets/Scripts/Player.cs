c8a9728b7f647cc6c268f0b45baf35ec1d05d8f7
Sinkorty <1103829278@qq.com>
2026-01-29 16:25:31 +0800

    实现了ClearCounter, Interactions, Selected/UnSelected

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    //public static Player instace;
+    //public static Player GetPlayer() => instace;
+    //public static void SetPlayer(Player player)
+    //{
+    //    instace = player;
+    //}
+    public static Player Instance { get; private set; }
+
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
+    private Vector3 lastInteractDir;
+    private ClearCounter selectedCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There is more than one Player instance");
+        }
+        Instance = this;
+    }
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e)
+    {
+        selectedCounter?.Interact();
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+    private void HandleMovement()
     {
         Vector2 playerMovement = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(playerMovement.x, 0f, playerMovement.y);
@@ -25,7 +72,7 @@ public class Player : MonoBehaviour
         if (!canMove)
         {
             // 尝试只在x轴上移动
-            Vector3 moveDirX = new Vector3(playerMovement.x, 0f, 0f);
+            Vector3 moveDirX = new Vector3(playerMovement.x, 0f, 0f).normalized;
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
             {
@@ -33,7 +80,7 @@ public class Player : MonoBehaviour
             }
             else // 尝试只在z轴上移动
             {
-                Vector3 moveDirZ = new Vector3(0f, 0f, playerMovement.y);
+                Vector3 moveDirZ = new Vector3(0f, 0f, playerMovement.y).normalized;
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
                 if (canMove)
                 {
@@ -56,9 +103,34 @@ public class Player : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
-
-    public bool IsWalking()
+    private void HandleInteractions()
     {
-        return isWalking;
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 faceDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        if (faceDir != Vector3.zero)
+        {
+            lastInteractDir = faceDir;
+        }
+
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                // Has ClearCounter
+                if (clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }
+            }
+            else SetSelectedCounter(null);
+        }
+        else SetSelectedCounter(null);
+    }
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs { selectedCounter = selectedCounter });
     }
 }
