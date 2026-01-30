d97354342ead364337d73cc3c15b8f9b40c54467
Sinkorty <1103829278@qq.com>
2026-01-30 17:08:27 +0800

    为CuttingCounter添加了动画，以及进度条

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,13 +7,31 @@ public class CuttingCounter : BaseCounter
 {
     [SerializeField] private CuttingRecipeSO[] cuttingRecipeSOArray;
 
+    public event EventHandler<OnProgressChangedEvnetArgs> OnProgressChanged;
+    public class OnProgressChangedEvnetArgs : EventArgs
+    {
+        public float progressNormalized;
+    }
+    public event EventHandler OnCut;
+
+
+    private int cuttingProgress;
+
     public override void Interact(Player player)
     {
-        if (player.HasKitchenObject() && !HasKitchenObject()) // 拿
+        if (player.HasKitchenObject() && !HasKitchenObject()) // 放
         {
             if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO())) // 如果手上拿的是可切的才让放
             {
+                cuttingProgress = 0;
                 player.GetKitchenObject().SetKitchenObjectParent(this);
+
+                // 设置progress的visual事件调用（解耦）
+                CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeWithInput(GetKitchenObject().GetKitchenObjectSO());
+                OnProgressChanged?.Invoke(this, new OnProgressChangedEvnetArgs
+                {
+                    progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingProgressMax,
+                });
             }
         }
         else if (!player.HasKitchenObject() && HasKitchenObject()) // 取
@@ -28,31 +47,48 @@ public class CuttingCounter : BaseCounter
 
             if (outputKitchenObjectSO != null) // CuttingCounter上放的是可切的才切
             {
-                GetKitchenObject().DestroySelf();
-                KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+                cuttingProgress++;
+                CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeWithInput(GetKitchenObject().GetKitchenObjectSO());
+
+                if (cuttingProgress >= cuttingRecipeSO.cuttingProgressMax)
+                {
+                    GetKitchenObject().DestroySelf();
+                    KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+                }
+
+                // NOTE: 视觉相关的代码就放在后面啦
+                OnProgressChanged?.Invoke(this, new OnProgressChangedEvnetArgs
+                {
+                    progressNormalized = (float)cuttingProgress / cuttingRecipeSO.cuttingProgressMax,
+                });
+
+                OnCut?.Invoke(this, EventArgs.Empty);
             }
         }
     }
     private KitchenObjectSO GetOutputFromInput(KitchenObjectSO kitchenObjectSO)
     {
-        foreach (var item in cuttingRecipeSOArray)
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeWithInput(kitchenObjectSO);
+        if (cuttingRecipeSO != null)
         {
-            if (kitchenObjectSO == item.input)
-            {
-                return item.output;
-            }
+            return cuttingRecipeSO.output;
         }
         return null;
     }
     private bool HasRecipeWithInput(KitchenObjectSO kitchenObjectSO)
     {
+        CuttingRecipeSO cuttingRecipeSO = GetCuttingRecipeWithInput(kitchenObjectSO);
+        return cuttingRecipeSO != null;
+    }
+    private CuttingRecipeSO GetCuttingRecipeWithInput(KitchenObjectSO kitchenObjectSO)
+    {
         foreach (var item in cuttingRecipeSOArray)
         {
             if (kitchenObjectSO == item.input)
             {
-                return true;
+                return item;
             }
         }
-        return false;
+        return null;
     }
 }
